918cd354e253b679c62c157f6bc6bbcee99f4e1b
변현석 <bhs3066@gmail.com>
2024-06-30 18:42:40 +0900

    week13과제

---
+++ week13/week13-prac/src/components/layout/Layout.jsx
@@ -0,0 +1,77 @@
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+import { Button } from "./common";
+import { ThemeColorContext } from "../../context/context";
+import { useRecoilValue } from "recoil";
+import { emailAtom, isSubmitedAtom, userNameAtom } from "../../recoil/atom";
+
+const Layout = ({ children }) => {
+  const context = useContext(ThemeColorContext);
+
+  const [mode, setMode] = useState(context.blueTheme);
+  const userName = useRecoilValue(userNameAtom);
+  const email = useRecoilValue(emailAtom);
+  const isSubmited = useRecoilValue(isSubmitedAtom);
+
+  const handleMode = (e) => {
+    const value = e.target.value;
+    if (value === "blue") {
+      setMode(context.blueTheme);
+    } else if (value === "green") {
+      setMode(context.greenTheme);
+    } else {
+      setMode(context.pinkTheme);
+    }
+  };
+  return (
+    <ThemeColorContext.Provider value={mode}>
+      <Wrapper>
+        <Header mode={mode.main}>
+          <Button onClick={handleMode} value="blue">
+            blue
+          </Button>
+          <Button onClick={handleMode} value="green">
+            green
+          </Button>
+          <Button onClick={handleMode} value="pink">
+            pink
+          </Button>
+        </Header>
+        <div>{children}</div>
+        <Footer mode={mode.main}>
+          {isSubmited ? `${userName} | ${email}` : "LikeLion FE"}
+        </Footer>
+      </Wrapper>
+    </ThemeColorContext.Provider>
+  );
+};
+
+export default Layout;
+
+const Wrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Header = styled.div`
+  display: flex;
+  height: 100px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.mode};
+`;
+
+const Footer = styled.div`
+  display: flex;
+  height: 50px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.mode};
+`;
